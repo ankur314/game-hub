5dbee38aed6056bcc414fd6cf217afb200f42f59
ankur314 <ankurojha314@gmail.com>
2024-06-16 22:42:21 +0530

    Adding descriptive comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ function App() {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
+        base: `"nav" "main"`, // Base type is mobile devices
         lg: `"nav nav" "aside main"`, // Devices wider than 1024 px
       }}
     >
       <GridItem area="nav" bg="coral">
         Nav
       </GridItem>
+      {/* Show this component for devices above lg size */}
       <Show above="lg">
         <GridItem area="aside" bg="gold">
           Aside
