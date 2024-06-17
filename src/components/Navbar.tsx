d393f5869742ba4bb024c23c60566dbc0a772c45
ankur314 <ankurojha314@gmail.com>
2024-06-17 15:05:48 +0530

    Build search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const Navbar = () => {
   return (
     // Layout component Horizontally. If you add one component after another, they will appear horizontally right next to each other
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px"></Image>
-      <ColorModeSwitch></ColorModeSwitch>
+      <SearchInput />
+      <ColorModeSwitch />
     </HStack>
   );
 };
